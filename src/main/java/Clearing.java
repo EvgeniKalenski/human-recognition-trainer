2e671e48aa4a77002ef302047294de8fac5d1b47
Yauheni Kalenski <Yauheni_Kalenski@epam.com>
2016-09-26 18:13:56 +0300

    refactor clearing

--- src/main/java/Clearing.java
+++ src/main/java/Clearing.java
@@ -2,6 +2,9 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 import static org.opencv.imgcodecs.Imgcodecs.imread;
 
@@ -11,32 +14,39 @@ public class Clearing {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         File womenData = new File("women/data");
-        new File("trainingFaces").mkdir();
+        File trainingFaces = new File("trainingFaces");
+        trainingFaces.mkdir();
+        Files.newDirectoryStream( trainingFaces.toPath() ).forEach( file -> {
+            try {
+                Files.delete( file );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } );
         File ugly = new File("ugly");
+        ugly.mkdir();
         outer:
         for (File file : womenData.listFiles()) {
+            Mat questionImg = imread(file.getPath());
+            if (questionImg.empty()) {
+                System.out.println("Empty image: " + file.getPath());
+                continue;
+            }
             for (File uglyFile : ugly.listFiles()) {
                 Mat uglyImg = imread(uglyFile.getPath());
-                Mat questionImg = imread(file.getPath());
-                if (questionImg.empty()) {
-                    System.out.println("Empty image: " + file.getPath());
-                    file.delete();
-                    continue outer;
-                }
                 Mat dst = new Mat(questionImg.size(), questionImg.type());
                 if (uglyImg.size().equals(questionImg.size())) {
-                    Core.bitwise_xor(uglyImg, questionImg, dst);
-                    if (!dst.empty()) {
-                        file.delete();
+                    Core.bitwise_xor(uglyImg, questionImg, dst); //
+                    if (!dst.empty()) { // todo need to improve condition
                         System.out.println("Broken image: " + file.getPath());
                         continue outer;
                     }
                 }
             }
-            file.renameTo(new File("trainingFaces/1-" + file.getName()));
+            Files.copy(file.toPath(), new File("trainingFaces/1-" + file.getName()).toPath(), REPLACE_EXISTING);
         }
     }
 
